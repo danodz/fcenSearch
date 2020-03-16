1e63327ee303d45b2ebd78a05627781fa9e3ea74
Erwan <erwan.bourdeau@gmail.com>
2020-03-16 09:11:09 -0400

    Moved fcenUpdate code to it's own file. Still made to run in browser, will change to run in node some other time.

---
+++ fcenUpdate.js
@@ -0,0 +1,29 @@
+var data = {};
+
+var foodRequest = new XMLHttpRequest();
+foodRequest.open("GET", "https://aliments-nutrition.canada.ca/api/fichier-canadien-elements-nutritifs/food/?lang=fr&type=json", true);
+foodRequest.onload = function (e) {
+    if (foodRequest.readyState === 4 && foodRequest.status === 200)
+    {
+        foodData = JSON.parse(foodRequest.responseText);
+        for(var i = 0; i<foodData.length; i++)
+        {
+            data[foodData[i].food_code] = {name : foodData[i].food_description, nutrients:{}};
+        }
+    }
+}
+foodRequest.send(null);
+
+var nutRequest = new XMLHttpRequest();
+nutRequest.open("GET", "https://aliments-nutrition.canada.ca/api/fichier-canadien-elements-nutritifs/nutrientamount/?lang=fr&type=json", true);
+nutRequest.onload = function (e) {
+    if (nutRequest.readyState === 4 && nutRequest.status === 200)
+    {
+        nutData = JSON.parse(nutRequest.responseText);
+        for(var i = 0; i<nutData.length; i++)
+        {
+            data[nutData[i].food_code].nutrients[nutData[i].nutrient_web_name] = nutData[i].nutrient_value;
+        }
+    }
+}
+nutRequest.send(null);
