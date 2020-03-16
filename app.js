759e8d7f01f467e98ffcd0146de8a419a0daf515
Erwan <erwan.bourdeau@gmail.com>
2020-03-16 09:02:44 -0400

    Single page contains all fcen info.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const fs = require('fs');
+const mustache = require('mustache');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const data = fs.readFileSync('fcen.json')
+const fcen = JSON.parse(fs.readFileSync('fcen.json'));
 
 const server = http.createServer((req, res) => {
     url = new URL(req.url, `http://${req.headers.host}`);
@@ -15,6 +16,10 @@ const server = http.createServer((req, res) => {
     else if(url.pathname == "/fcen/search")
     {
         fcenSearch(res);
+    }
+    else if(url.pathname == "/fcen/searchStatic")
+    {
+        fcenSearchStatic(res);
     }
     else
     {
@@ -32,10 +37,50 @@ function fcenJson(res)
 
 function fcenSearch(res)
 {
-    var page = fs.readFileSync('index.html')
+    var body = "";
+    var alimentTemplate = fs.readFileSync("aliment.html").toString();
+    var nutrientTemplate = fs.readFileSync("nutrient.html").toString();
+    
+    for(id in fcen)
+    {
+        var nutrients = "";
+        for(nutrient in fcen[id].nutrients)
+        {
+            nutrients += mustache.render(nutrientTemplate, {name : nutrient, value: fcen[id].nutrients[nutrient]});
+        };
+        body += mustache.render(alimentTemplate, {name : fcen[id].name, nutrients : nutrients});
+    }
 
+    var page = htmlPage(["script.js"], ["styles.css"], "", body);
+    fs.writeFileSync("fcen.html", page);
     res.statusCode = 200;
     res.end(page);
+}
+
+function fcenSearchStatic(res)
+{
+    res.statusCode = 200;
+    res.end(fs.readFileSync("fcen.html"));
+}
+
+function htmlPage(scripts, styles, head, body)
+{
+    var content = {"scripts" : "", "styles" : "", "head" : head, "body": body};
+
+    for(script of scripts)
+    {
+        content["scripts"] += fs.readFileSync(script).toString();
+    }
+    for(style of styles)
+    {
+        content["styles"] += fs.readFileSync(style).toString();
+    }
+
+    var page = fs.readFileSync('template.html').toString();
+    page = mustache.render(page, content);
+    return page;
+    //<script src="script.js"></script>
+    //<link rel="stylesheet" type="text/css" href="styles.css">
 }
 
 server.listen(port, hostname, () => {
