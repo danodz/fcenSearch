7135421982b11eb2c8e463136f3283e8b868cc50
Erwan <erwan.bourdeau@gmail.com>
2020-03-22 09:47:41 -0400

    nutrient filter functionally done

--- fcenClientLowGen.js
+++ fcenClientLowGen.js
@@ -18,7 +18,7 @@ function toggleNuts()
     var id = root.id
     var nutrients = "";
     var nutrition = root.getElementsByClassName("nutrition")[0];
-    if(root.getElementsByClassName("nutrient").length == 0)
+    if(root.getElementsByClassName("nutrition")[0].getElementsByClassName("nutrient").length == 0)
     {
         for(nutId in fcen[id].nutrients)
         {
@@ -43,7 +43,7 @@ function filterFoodName()
     var caseSensible = document.getElementsByClassName("caseSensitivity")[0].checked;
     var search = caseSensible? document.getElementsByClassName("searchInput")[0].value:document.getElementsByClassName("searchInput")[0].value.toLowerCase();
     search = search.split(/[\s,]+/);
-    for(id in fcen)
+    for(var id in fcen)
     {
         var name = caseSensible? fcen[id].name:fcen[id].name.toLowerCase();
         var match = true;
@@ -74,16 +74,35 @@ function showNutrient()
     {
         if(fcen[i].nutrients[nutId])
         {
-            var nutrient = fcen[i].nutrients[nutId];
-            var nutData = { name : nutrient.name, value : nutrient.value, id: nutId}
-            document.getElementById(i).getElementsByClassName("nutritionShow")[0].innerHTML += Mustache.render(nutrientTemplate, nutData);
+            var nutNode = document.getElementById(i).getElementsByClassName("nutritionShow")[0].getElementsByClassName(nutId)[0];
+            if(nutNode)
+            {
+                nutNode.classList.toggle("hidden");
+            }
+            else
+            {
+                var nutrient = fcen[i].nutrients[nutId];
+                var nutData = { name : nutrient.name, value : nutrient.value, id: nutId}
+                document.getElementById(i).getElementsByClassName("nutritionShow")[0].innerHTML += Mustache.render(nutrientTemplate, nutData);
+            }
         }
     }
 }
 
 function hideNutrient()
 {
-    var id = event.target.parentNode.parentNode.id;
+    var nutId = event.target.parentNode.parentNode.getAttribute("data-nutid");
+    if (event.target.checked)
+    {
+        var node = document.createElement('style');
+        node.className=nutId;
+        node.innerHTML=".aliment .nutrition ."+ nutId +" {display:none}";
+        document.head.appendChild(node);
+    }
+    else
+    {
+        document.head.getElementsByClassName(nutId)[0].remove();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
